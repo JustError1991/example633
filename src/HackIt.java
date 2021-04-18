45fea48ee7811fc1864ab567669756d853764ddc
JustError <9162367422@mail.ru>
2021-04-18 12:07:31 +0300

    first

---
+++ src/HackIt.java
@@ -0,0 +1,21 @@
+import anotherfirm.*;
+
+import java.util.Arrays;
+
+public class HackIt extends Secret{
+
+    public HackIt(){
+        super.getSecret();
+    }
+
+    public static void main(String[] args) {
+        // Call the method getSecret() of class anotherfirm.Secret
+        // from package "anotherfirm" here
+        // The output will be the code to the excercise
+        // ...
+//        Secret secret = new Secret();
+//        secret.getSecret();
+        HackIt secret = new HackIt();
+        Arrays
+    }
+}
